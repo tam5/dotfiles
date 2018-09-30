c570b888f43407daec511c723280f48dd74e00ee
tam5 <arimiller92@gmail.com>
2018-09-30 13:16:19 -0400

    rename for better editor support

--- scripts/slack.sh
+++ scripts/slack.sh
@@ -9,7 +9,7 @@ cd "$setup_dir"
 slack_theme_location=/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
 
 # for atom one TODO add more choices
-theme_file=../slack/ssb-interop.js.atom-one-dark
+theme_file=../slack/ssb-interop.atom-one-dark.js
 
 sudo cp $theme_file $slack_theme_location
 
