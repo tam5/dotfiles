7191972a1ddab927423e03d8e9ba777578f7401f
tam5 <arimiller92@gmail.com>
2023-05-09 13:26:58 -0400

    few more things

--- config/zsh/+git.zsh
+++ config/zsh/+git.zsh
@@ -4,6 +4,9 @@
 
 alias gs="git status"
 
+alias wip="git add . && git commit -m 'wip' --no-verify"
+alias nah="git reset --hard head && git clean -fd"
+alias gpop="git reset --soft head~1 && git reset head"
 
 ### TODO make these smarter functions
 alias ga="git add"
@@ -12,4 +15,3 @@ alias gc="git commit"
 alias gp="git push"
 alias gd="git diff"
 alias gco="git checkout"
-alias nah="git reset --hard head && git clean -fd"
