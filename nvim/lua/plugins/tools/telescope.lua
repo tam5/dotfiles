881fbcbbd3c8e9d74435a034811906845eb96da5
Ari Miller <arimiller92@gmail.com>
2025-10-31 12:45:44 -0400

    update telescope find files

--- nvim/lua/plugins/tools/telescope.lua
+++ nvim/lua/plugins/tools/telescope.lua
@@ -8,11 +8,18 @@ return {
                     prompt_position = 'top'
                 },
                 sorting_strategy = 'ascending'
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    no_ignore = false,
+                    no_ignore_parent = false,
+                }
             }
         }
     end,
     keys = {
-        { "<C-p>", "<cmd>Telescope git_files<cr>", desc = "FindFiles" },
+        { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "FindFiles" },
         { "<space>sp", "<cmd>Telescope live_grep<cr>", desc = "Project Search" },
         { "<space>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
     }
